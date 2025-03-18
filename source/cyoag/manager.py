74aeef4fcb5dc5cba0a18f9ee9db8e0d48913044
8leak <callumchristian98@gmail.com>
2025-03-18 20:30:23 +0000

    Consolidate data_models, refactor data importing into Manager

--- source/cyoag/manager.py
+++ source/cyoag/manager.py
@@ -1,47 +1,45 @@
 import logging
 from typing import List
-import json
 import click
 import input
-import pathlib
-import os
 from typing import Dict, List
 from pydantic import BaseModel, ConfigDict
 
 from player import Player
-# from rooms import Room, rooms
 from data_models import Choice, Item, Room
 
+import json
+from pathlib import Path
 
 class Manager:
     def __init__(self, player: Player) -> None:
-        self.rooms: Dict[str, Room]
-        self.location: Room
-        # self.items: List[str] = self.location.items
+        self.location: Room = "room"
         self.items: List[str]
         self.player: Player = player
+        self.rooms: Dict[str, Room]
     
     def _load_data(self) -> None:
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        json_path = os.path.join(current_dir, "data")
+        current_dir = Path(__file__).resolve().parent
+        json_path = current_dir / "data"
 
-        with open(json_path, "r") as file:
+        with open(json_path/"choices.json", "r") as file:
             choices_data: List[Choice] = [Choice(**choices) for choices in json.load(file)]
-        self.choices: Dict[str, Choice] = {choices.name: choices for choices in choices_data}
+        choices_dict: Dict[str, Choice] = {choices.name: choices for choices in choices_data}
 
-        with open(json_path, "r") as file:
+        with open(json_path/"items.json", "r") as file:
             items_data: List[Item] = [Item(**items) for items in json.load(file)]
-        self.items: Dict[str, Item] = {items.name: items for items in items_data}
+        items_dict: Dict[str, Item] = {items.name: items for items in items_data}
 
-        with open(json_path, "r") as file:
+        with open(json_path/"rooms.json", "r") as file:
             rooms_data: List[Room] = [Room(**room) for room in json.load(file)]
 
         self.rooms: Dict[str, Room] = {room.name: room for room in rooms_data}
+        
         for room in self.rooms.values():
-            room.items = {item: self.items[item] for item in room.item_list}
-            room.choices = {choice: self.choices[choice] for choice in room.choice_list}
-        self.location: Room = self.rooms["start"]
+            room.items = {item: items_dict[item] for item in room.item_list}
+            room.choices = {choice: choices_dict[choice] for choice in room.choice_list}
 
+        self.location: Room = self.rooms["start"]
 
     def start():
         pass
@@ -64,14 +62,14 @@ class Manager:
 
     def update_location(self, exit: str) -> None:
         logging.info(f"(manager.py) Updating manager.location to: {exit}")
-        self.location = rooms[exit]
+        self.location = self.rooms[exit]
         self.items = self.location.items
         logging.info(f"(manager.py) manager.location successfully updated to: {exit}")
 
     def update_items(self, current_room: Room, item: str, player, action: str) -> None:
         if action == "take":
             player.add_item(current_room, item)
-            self.items.pop(item)
+            self.location.items.pop(item)
 
         elif action == "drop":
-            self.items[item] = player.drop_item(current_room, item)
+            self.location.items[item] = player.drop_item(current_room, item)
